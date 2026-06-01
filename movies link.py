1f28a3516bc61ba0da8d6e24482b4acb683b126c
piyushcodevns <168630881+piyushcodevns@users.noreply.github.com>
2026-06-01 10:17:52 +0530

    Create movies link.py

---
+++ movies link.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+}
+url = "https://en.wikipedia.org/wiki/Cinema_of_India"
+response = requests.get(url, headers=headers)
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text, "html.parser")
+    links = soup.find_all("a")
+    print(f"Total {len(links)} links.")
+else:
+    print(f"Error! Status Code: {response.status_code}")
